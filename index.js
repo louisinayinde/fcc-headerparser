c6bd696cd7aa7702c546c96e677682c2f22ec3f8
louisinayinde <louisinayinde@gmail.com>
2024-04-05 06:26:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,11 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get("/api/whoami", function (req, res) {
-  
-  const responseObj = {
+  res.json({
     "ipaddress": req.ip,
     "language": req.headers["accept-language"],
     "software": req.headers["user-agent"]
-  };
-
-  res.json(responseObj);
+  });
 
 });
 
